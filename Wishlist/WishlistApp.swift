18ad5eccbac1730e4b4fbb9c27a08ad806539ad7
Preeteesh Remalli <priteeshh@gmail.com>
2026-01-04 22:38:22 +0400

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Preeteesh Remalli on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
