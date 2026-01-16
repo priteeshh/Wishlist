4e34944c164b3356f3283cb589c4e87c5b28e6ea
Preeteesh Remalli <priteeshh@gmail.com>
2026-01-16 20:58:08 +0400

    added wishlist project

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
+    
+    @State private var isAlerShowing: Bool = false
+    @State private var title: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(wishes) { wish in
+                Text(wish.title)
+                    .fontWeight(.medium)
+                    .padding(.vertical, 8)
+                    .swipeActions {
+                        Button("Delete", role: .destructive) {
+                            modelContext.delete(wish)
+                        }
+                    }
+            }
+            .navigationTitle("Wishlist")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isAlerShowing.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+
+            }
+            .alert("Add Wish", isPresented: $isAlerShowing) {
+                TextField("Enter a Wish", text: $title)
+                
+                Button {
+                    modelContext.insert(Wish(title: title))
+                    title = ""
+                } label: {
+                    Text("Save")
+                }
+            }
         }
-        .padding()
+        .overlay {
+            if wishes.isEmpty {
+                ContentUnavailableView("My Wishlist", systemImage: "heart.circle", description: Text("No wishes added yet. Add a wish to get started."))
+            }
+        }
     }
 }
 
-#Preview {
+#Preview("List with sample data") {
+    let container = try! ModelContainer(
+        for: Wish.self,
+        configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+    )
+    container.mainContext.insert(Wish(title: "Learn Swift UI"))
+    container.mainContext.insert(Wish(title: "Learn Swift Data"))
+    container.mainContext.insert(Wish(title: "Learn Combine"))
+    container.mainContext.insert(Wish(title: "Learn Animations"))
+
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }
